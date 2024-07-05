20e25c1f945e4b20c2bf83d925d80d01aa1c9a62
renan <renancruz199@gmail.com>
2024-07-05 20:28:31 -0300

    about me

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, HeaderComponent]
+    imports: [RouterOutlet, HeaderComponent,AboutMeComponent]
 })
 export class AppComponent {
   title = 'renan-portfolio';
