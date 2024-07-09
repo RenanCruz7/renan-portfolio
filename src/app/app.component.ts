c8f2908ba79c8516a0131d2f56d7f056dc627937
renan <renancruz199@gmail.com>
2024-07-09 11:58:50 -0300

    Ajustando home e iniciando desenvolvimento do about-me

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { HomeComponent } from './components/home/home.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, HeaderComponent,HomeComponent]
+    imports: [RouterOutlet, HeaderComponent,HomeComponent,AboutMeComponent]
 })
 export class AppComponent {
   title = 'renan-portfolio';
