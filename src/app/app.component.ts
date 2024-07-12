1ec9d06c72dae6d314c151bc88967498ce102ed6
renan <renancruz199@gmail.com>
2024-07-12 20:04:50 -0300

    Componente de experience finalizado e criação da parte de projects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { HomeComponent } from './components/home/home.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ExperienceComponent } from "./components/experience/experience.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, HeaderComponent,HomeComponent,AboutMeComponent]
+    imports: [RouterOutlet, HeaderComponent, HomeComponent, AboutMeComponent, ExperienceComponent]
 })
 export class AppComponent {
   title = 'renan-portfolio';
