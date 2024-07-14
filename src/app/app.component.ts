08d66de81ff52a94ef71581416e78a2b5e5e1711
renan <renancruz199@gmail.com>
2024-07-14 11:56:32 -0300

    Seção de projetos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { HeaderComponent } from "./components/header/header.component";
 import { HomeComponent } from './components/home/home.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
 import { ExperienceComponent } from "./components/experience/experience.component";
+import { ProjectsComponent } from "./components/projects/projects.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, HeaderComponent, HomeComponent, AboutMeComponent, ExperienceComponent]
+    imports: [RouterOutlet, HeaderComponent, HomeComponent, AboutMeComponent, ExperienceComponent, ProjectsComponent]
 })
 export class AppComponent {
   title = 'renan-portfolio';
